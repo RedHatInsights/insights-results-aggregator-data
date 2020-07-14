e2760cfeca1fd85a492fdeda17f1ba000918870a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-14 09:47:19 +0200

    Fix issue #14

--- testdata/testdata.go
+++ testdata/testdata.go
@@ -27,6 +27,7 @@ import (
 )
 
 const (
+	// OrgID represents ID of the first organization in the system (ID=1)
 	OrgID            = types.OrgID(1)
 	ClusterName      = types.ClusterName("84f7eedc-0dd8-49cd-9d4d-f6646df3a5bc")
 	UserID           = types.UserID("1")
