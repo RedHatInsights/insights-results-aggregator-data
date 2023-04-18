0b689074bf3f6fcc2f0b742a3c9a608dd37b770d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-18 11:37:10 +0200

    Reformatted to use standardized EDN format

--- messages/invalid_wrong_cluster_name/no_hits_no_skips.clj
+++ messages/invalid_wrong_cluster_name/no_hits_no_skips.clj
@@ -1,10 +1,5 @@
 {"ClusterName" "invalid-cluster-name",
  "LastChecked" "2020-04-02T09:00:05.268294Z",
  "OrgID" 11789772,
- "Version" 1,
- "Report" {"fingerprints" [],
-           "info" [],
-           "pass" [],
-           "reports" [],
-           "skips" [],
-           "system" {"hostname" nil, "metadata" {}}}}
+ "Report" {"fingerprints" [], "info" [], "pass" [], "reports" [], "skips" [], "system" {"hostname" nil, "metadata" {}}},
+ "Version" 1}
