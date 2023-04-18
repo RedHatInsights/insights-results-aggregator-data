224076515076b20f1e05def49fb3b4a2b7c7939d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-18 10:00:16 +0200

    Reformatted to use standardized EDN format

--- messages/invalid_no_version/no_hits_no_skips.clj
+++ messages/invalid_no_version/no_hits_no_skips.clj
@@ -1,10 +1,5 @@
-{"ClusterName" "5d5892d3-1f74-4ccf-91af-548dfc9767aa",
+{"AccountNumber" 10,
+ "ClusterName" "5d5892d3-1f74-4ccf-91af-548dfc9767aa",
  "LastChecked" "2020-04-02T09:00:05.268294Z",
  "OrgID" 11789772,
- "AccountNumber" 10,
- "Report" {"fingerprints" [],
-           "info" [],
-           "pass" [],
-           "reports" [],
-           "skips" [],
-           "system" {"hostname" nil, "metadata" {}}}}
+ "Report" {"fingerprints" [], "info" [], "pass" [], "reports" [], "skips" [], "system" {"hostname" nil, "metadata" {}}}}
