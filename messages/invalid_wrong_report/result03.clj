972f3f7c0b9c95be6c5a5abb1a7c2528d2b8d2ba
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-12 08:04:40 +0200

    Reformatted to use standardized EDN format

--- messages/invalid_wrong_report/result03.clj
+++ messages/invalid_wrong_report/result03.clj
@@ -1,6 +1,5 @@
 {"ClusterName" "22222222-bbbb-cccc-dddd-ffffffffffff",
  "LastChecked" "2020-01-23T16:15:59.478901889Z",
  "OrgID" 2,
- "Version" 1,
- "Report" "wrong-report"
-}
+ "Report" "wrong-report",
+ "Version" 1}
